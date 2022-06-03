849b092122908ae417a35308a7f30517a10227e9
Dux93 <dux.capy.93@gmail.con>
2022-06-03 03:29:10 -0500

    changes for termux

--- cmd/tfidf/main.go
+++ cmd/tfidf/main.go
@@ -27,8 +27,47 @@ func print_doc(url string) {
 	}
 }
 
-func document(url, word string) int {
+func wordAllDocument(url string) []string
+{
+  var word words string
+   file, mistake := os.Open(url)
+    defer file.Close()
+	if mistake != nil {
+		fmt.Println("At ocurred a Mistake")
+	}
+	scanner := bufio.NewScanner(file)
+	var i, c,k int
 
+	for scanner.Scan() {
+		var fields []string
+		i++
+		line := scanner.Text()
+
+		fields = strings.Fields(line)
+		fmt.Println(fields)
+		for i := 0; i < len(fields); i++ {
+		    if strings.Contains(line, fields[i]) {
+				if strings.EqualFold(fields[i], fields[i]) {
+				    k++
+				    if(k>=1){
+				        word = fields[i]
+				        c++
+				    }
+				}
+			}
+		}
+		
+words += word + string(c) + "/n"
+	}
+	return words
+}
+			
+
+
+
+
+func document(url, word string) int {
+   
 	file, mistake := os.Open(url)
 	defer file.Close()
 	if mistake != nil {
@@ -59,6 +98,8 @@ func document(url, word string) int {
 	return c
 }
 
+
+/*
 func main() {
 	var word string
 	print_doc("data/document_1.txt")
@@ -77,7 +118,11 @@ func main() {
 
 	fmt.Println("total repeated in all documents: " + fmt.Sprint(d))
 }
+*/
 
-func stringintconv(d int) {
-	panic("unimplemented")
+
+func main() {
+	wordAllDocument("data/document_1.txt")
+	wordAllDocument("data/document_2.txt")
+	wordAllDocument("data/document_3.txt")
 }
