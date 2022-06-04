07cc2b3d0300db7553fc084dc7bd39893fb1d8c2
Dux93 <dux.capy.93@gmail.com>
2022-06-03 20:57:12 -0500

    back test

--- cmd/tfidf/main.go
+++ cmd/tfidf/main.go
@@ -7,7 +7,28 @@ import (
 	"strings"
 )
 
-func print_doc(url string) {
+func main() {
+
+	var word string
+	fmt.Println(print_doc("data/document_1.txt"))
+	fmt.Println(print_doc("data/document_2.txt"))
+	fmt.Println(print_doc("data/document_3.txt"))
+
+	fmt.Println("From the Documents, type the word that you notice is repeated: ")
+	fmt.Scanln(&word)
+	a := document("data/document_1.txt", word)
+	b := document("data/document_2.txt", word)
+	c := document("data/document_3.txt", word)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	d := a + b + c
+	fmt.Println("total repeated in all documents: " + fmt.Sprint(d))
+
+}
+
+func print_doc(url string) string {
+	var full string
 	file, mistake := os.Open(url)
 	defer file.Close()
 	if mistake != nil {
@@ -15,58 +36,16 @@ func print_doc(url string) {
 	}
 	scanner := bufio.NewScanner(file)
 	var i int
-
 	for scanner.Scan() {
-		var fields []string
 		i++
 		line := scanner.Text()
-
-		fields = strings.Fields(line)
-		fmt.Println(fields)
-
+		full += line + " \n"
 	}
+	return full
 }
-
-func wordAllDocument(url string) string{
-  var word, words string
-   file, mistake := os.Open(url)
-    defer file.Close()
-	if mistake != nil {
-		fmt.Println("At ocurred a Mistake")
-	}
-	scanner := bufio.NewScanner(file)
-	var i, c,k int
-
-	for scanner.Scan() {
-		var fields []string
-		i++
-		line := scanner.Text()
-
-		fields = strings.Fields(line)
-		fmt.Println(fields)
-		for i := 0; i < len(fields); i++ {
-		    if strings.Contains(line, fields[i]) {
-				if strings.EqualFold(fields[i], fields[i]) {
-				    k++
-				    if(k>=1){
-				        word = fields[i]
-				        c++
-				    }
-				}
-			}
-		}
-		
-words += word + string(c) + "/n"
-	}
-	return words
-}
-			
-
-
-
 
 func document(url, word string) int {
-   
+
 	file, mistake := os.Open(url)
 	defer file.Close()
 	if mistake != nil {
@@ -81,7 +60,7 @@ func document(url, word string) int {
 		line := scanner.Text()
 
 		fields = strings.Fields(line)
-		fmt.Println(fields)
+
 		for i := 0; i < len(fields); i++ {
 
 			if strings.Contains(line, fields[i]) {
@@ -93,35 +72,5 @@ func document(url, word string) int {
 		}
 
 	}
-
 	return c
-}
-
-
-/*
-func main() {
-	var word string
-	print_doc("data/document_1.txt")
-	print_doc("data/document_2.txt")
-	print_doc("data/document_3.txt")
-	fmt.Println("From the Documents, type the word that you notice is repeated: ")
-	fmt.Scanln(&word)
-	a := document("data/document_1.txt", word)
-	b := document("data/document_2.txt", word)
-	c := document("data/document_3.txt", word)
-
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	d := a + b + c
-
-	fmt.Println("total repeated in all documents: " + fmt.Sprint(d))
-}
-*/
-
-
-func main() {
-	wordAllDocument("data/document_1.txt")
-	wordAllDocument("data/document_2.txt")
-	wordAllDocument("data/document_3.txt")
 }
